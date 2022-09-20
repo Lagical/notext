2921ad4ee314ae291cb26e8c87518c23ea7eaf35
Lagical <70884073+Lagical@users.noreply.github.com>
2022-09-20 20:31:45 +0300

    filujen setviminen

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { RecordingData, VoiceRecorder } from 'capacitor-voice-recorder';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+
+  recording = false;
+  storedFileNames = [];
 
   constructor() {}
+
+  ngOnInit(){
+    this.loadFiles();
+
+    VoiceRecorder.requestAudioRecordingPermission();
+  }
+
+  async loadFiles(){
+    Filesystem.readdir({
+      path: '',
+      directory: Directory.Data
+    }).then(result =>{
+      console.log(result);
+      this.storedFileNames = result.files;
+    });
+  }
+
+  startRecording(){
+    if (this.recording){
+      return;
+    }
+    this.recording = true;
+    VoiceRecorder.startRecording();
+  }
+
+  stopRecording(){
+    if (!this.recording){
+      return;
+    } 
+    VoiceRecorder.stopRecording().then(async (result: RecordingData) => {
+      if(result.value && result.value.recordDataBase64){
+        const recordData = result.value.recordDataBase64;
+        console.log("toimiiiii");
+        const fileName = new Date().getTime() + ".wav";
+        await Filesystem.writeFile({
+          path: fileName,
+          directory: Directory.Data,
+          data: recordData
+        });
+        this.loadFiles();
+      }
+    }
+  }
 
 }
